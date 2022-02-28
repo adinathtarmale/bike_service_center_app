0c121ce3a6e46c3706dc9edf0ddf8025de043bf1
adinathtarmale <59080897+adinathtarmale@users.noreply.github.com>
2022-02-28 19:12:16 +0530

    Update MyConnection.java

--- src/dao/MyConnection.java
+++ src/dao/MyConnection.java
@@ -1,11 +1,12 @@
 package dao;
 import java.sql.*;
 
+// creating the connection with the database
 public class MyConnection {
     public static Connection connect(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/BikeService?zeroDateTimeBehavior=CONVERT_TO_NULL","serviceadmin","serviceadmin");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/BikeService?zeroDateTimeBehavior=CONVERT_TO_NULL","databasename","password");
             return con;
         }
         catch(ClassNotFoundException | SQLException e){
